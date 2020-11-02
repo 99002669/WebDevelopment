172093349856817d86fbc8d30c66ce52d99f1e46
99002669 <71541637+99002669@users.noreply.github.com>
2020-11-02 11:28:17 +0530

    classdemo

---
+++ NodeJS/classdemo.js
@@ -0,0 +1,9 @@
+const HOBBIES = ["sports","music","dance"]; 
+//HOBBIES = "Reading"; 
+HOBBIES.push("Reading"); 
+console.log(HOBBIES) 
+HOBBIES.pop(); 
+console.log(HOBBIES) // HOBBIES=[];
+ const student={name:'Ram',age:20} 
+ student.age=21;
+console.log(student);
